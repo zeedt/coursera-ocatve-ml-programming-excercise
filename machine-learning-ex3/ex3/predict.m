8fbfbd64997758885925cab439f88e19a27195ca
zeedt <yusufsaheedtaiwo@gmail.com>
2021-01-31 08:59:50 +0100

    completed neural network excercise

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -2,7 +2,9 @@ function p = predict(Theta1, Theta2, X)
 %PREDICT Predict the label of an input given a trained neural network
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
-
+X = [ones(length(X),1) X]
+%Theta1
+Theta2
 % Useful values
 m = size(X, 1);
 num_labels = size(Theta2, 1);
@@ -21,11 +23,24 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
+store=zeros(length(p),length(Theta1));
+for i=1:length(Theta1),
+    for j=1:length(X),
+        store(j,i)=sigmoid(X(j,:) * Theta1(i,:)');
+    end
+end
+store;
+store=[ones(length(store),1) store];
+zeros(length(store),length(Theta2));
+[rowSize,colSize] = size(Theta2);
+storeLatest=zeros(rowSize,colSize);
+for i=1:rowSize,
+   for j=1:length(store),
+      storeLatest(j,i)=sigmoid(store(j,:) * Theta2(i,:)');
+    end
+end
+[val,index] = max(storeLatest, [], 2);
+p=index;
 
 
 
